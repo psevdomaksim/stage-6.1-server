1378db59ca5de211e56e2ac0c41a304535e36049
psevdomaksim <varmax2004@gmail.com>
2024-10-12 03:40:28 +0300

    Add all stage 6.1 backend functionality

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const router = require("./routes/index");
+const sequelize = require("./db");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({ origin: process.env.CORS_URL }));
+app.use("/api", router);
+
+app.use("/", (req, res) => {
+  res.send("server is running.");
+});
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    app.listen(process.env.PORT, () =>
+      console.log(`server started listening on ${process.env.PORT}`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const Router = require("express");
+const router = new Router();
+
+const postRouter = require("./postRouter");
+
+router.use("/post", postRouter);
+
+module.exports = router;
